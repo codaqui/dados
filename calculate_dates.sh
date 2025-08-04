0034b4c42d052603058e1ee4e8518eb3fca67115
Enderson Menezes <endersonster@gmail.com>
2025-08-04 18:22:01 -0400

    fix: improve argument documentation in calculate_dates.sh

--- calculate_dates.sh
+++ calculate_dates.sh
@@ -6,13 +6,15 @@
 # Return = DD-MM-YYYY DD-MM-YYYY
 
 ARGUMENT=$1
+## Argument = MMYYYY
 if [[ $ARGUMENT =~ ^[0-9]{6}$ ]]; then
     # Extract month and year from MMYYYY format
     MONTH=${ARGUMENT:0:2}
     YEAR=${ARGUMENT:2:4}
     FIRST_DAY_MONTH=$(date -d "$YEAR-$MONTH-01" +%Y-%m-%d)
     LAST_DAY_MONTH=$(date -d "$FIRST_DAY_MONTH +1 month -1 day" +%Y-%m-%d)
-elif [[ $ARGUMENT == "last_month" ]]; then
+## Argument = last_month OR $GITHUB_EVENT_NAME = schedule
+elif [[ $ARGUMENT == "last_month" || $GITHUB_EVENT_NAME == "schedule" ]]; then
     FIRST_DAY_MONTH=$(date -d "$(date +%Y)-$(date +%m)-01 -1 month" +%Y-%m-%d)
     LAST_DAY_MONTH=$(date -d "$FIRST_DAY_MONTH +1 month -1 day" +%Y-%m-%d)
 else
